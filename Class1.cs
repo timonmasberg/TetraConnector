3a7888251e0be4cab7407df44f0dbb66bb2c9d7a
Timon Masberg <kontakt@timonmasberg.de>
2021-05-09 22:33:48 +0200

    Add abstract Logger and abstract Logger Event

--- Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TetraConnector
-{
-    public class Class1
-    {
-    }
-}
---
+++ Logs/LogEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TetraConnector.Logs
+{
+    public abstract class LogEvent
+    { 
+        protected string PrefixTitle = "";
+        protected string Message;
+
+        protected ConsoleColor ForeColor = ConsoleColor.White;
+        protected ConsoleColor BackgroundColor = ConsoleColor.Black;
+
+        protected LogEvent(string message)
+        {
+            this.Message = message;
+        }
+
+    }
+}
